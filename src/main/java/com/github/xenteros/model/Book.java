a1f663ee7b630b244e63b6a9d231ad00f3778620
xenteros <adam@gurgul.com.pl>
2019-04-09 21:08:21 +0200

    Create dummy-api for books (#2)

---
+++ src/main/java/com/github/xenteros/model/Book.java
@@ -0,0 +1,37 @@
+package com.github.xenteros.model;
+
+public class Book {
+
+    private Long id;
+    private String author;
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(Long id, String author, String title) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
