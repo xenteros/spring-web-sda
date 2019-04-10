e6c2f2f6e3207bf8ea4768e06b30544fc0be2c43
xenteros <adam@gurgul.com.pl>
2019-04-10 19:22:52 +0200

    Create BookDto
    * Created BookDto,
    * Implemented mapper,
    * Created another controller to work with BookDtos

--- src/main/java/com/github/xenteros/controller/ImprovedBookController.java
+++ src/main/java/com/github/xenteros/controller/ImprovedBookController.java
@@ -1,29 +1,39 @@
 package com.github.xenteros.controller;
 
 
+import com.github.xenteros.dto.BookDto;
+import com.github.xenteros.mapper.BookMapper;
 import com.github.xenteros.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/books")
-class BookController {
+@RequestMapping("/api/v2/books")
+public class ImprovedBookController {
 
+    private BookMapper bookMapper;
     private List<Book> books = new ArrayList<>();
     private Long nextId = 1L;
 
+    @Autowired
+    public ImprovedBookController(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
     @GetMapping
-    public List<Book> getAllBooks() {
-        return books;
+    public List<BookDto> getAllBooks() {
+        return bookMapper.toBookDto(books);
     }
 
     @GetMapping("/{id}")
-    public Book findBookById(@PathVariable Long id) {
+    public BookDto findBookById(@PathVariable Long id) {
         return books.stream()
                 .filter(book -> book.getId().equals(id))
                 .findFirst()
+                .map(bookMapper::toBookDto)
                 .orElseThrow(RuntimeException::new);
     }
 
@@ -33,20 +43,20 @@ class BookController {
     }
 
     @PostMapping
-    public Book createBook(@RequestParam String title, @RequestParam String author) {
-        Book book = new Book(nextId++, author, title);
+    public BookDto createBook(@RequestBody BookDto bookDto) {
+        Book book = new Book(nextId++, bookDto.getAuthor(), bookDto.getTitle());
         books.add(book);
-        return book;
+        return bookMapper.toBookDto(book);
     }
 
     @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id,
-                           @RequestParam String newAuthor,
-                           @RequestParam String newTitle) {
+    public BookDto updateBook(@PathVariable Long id,
+                              BookDto bookDto) {
         return books.stream()
                 .filter(book -> book.getId().equals(id))
                 .findFirst()
-                .map(book -> updateBook(book, newTitle, newAuthor))
+                .map(book -> updateBook(book, bookDto.getTitle(), bookDto.getAuthor()))
+                .map(bookMapper::toBookDto)
                 .orElseThrow(RuntimeException::new);
     }
 
