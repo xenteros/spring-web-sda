bfa5186a467750a817a974ac468e6aa7d3716e6b
xenteros <adam@gurgul.com.pl>
2019-04-15 20:15:52 +0200

    Add Swagger2 (#9)

--- src/main/java/com/github/xenteros/Application.java
+++ src/main/java/com/github/xenteros/Application.java
@@ -3,11 +3,39 @@ package com.github.xenteros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
 class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Book API")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api/v2.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .description("Simple REST API to learn")
+                .title("Rent a book")
+                .build();
+    }
+
+
 }
